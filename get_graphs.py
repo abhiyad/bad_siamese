9ac9e3041c4f59ee212c0f12ec8c8406df3e81ff
Abhishek Yadav <abhishekcms611@gmail.com>
2018-07-06 21:45:47 +0530

    Added script for true extracting graphs

---
+++ get_graphs.py
@@ -0,0 +1,257 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.layers import Input, Conv2D, Lambda, merge, Dense, Flatten,MaxPooling2D
+from keras.models import Model, Sequential
+from keras.regularizers import l2
+from keras import backend as K
+from keras.optimizers import SGD,Adam
+from keras.losses import binary_crossentropy
+import numpy.random as rng
+import numpy as np
+import os
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.utils import shuffle
+
+def W_init(shape,name=None):
+    """Initialize weights as in paper"""
+    values = rng.normal(loc=0,scale=1e-2,size=shape)
+    return K.variable(values,name=name)
+#//TODO: figure out how to initialize layer biases in keras.
+def b_init(shape,name=None):
+    """Initialize bias as in paper"""
+    values=rng.normal(loc=0.5,scale=1e-2,size=shape)
+    return K.variable(values,name=name)
+
+input_shape = (105, 105, 1)
+left_input = Input(input_shape)
+right_input = Input(input_shape)
+#build convnet to use in each siamese 'leg'
+convnet = Sequential()
+convnet.add(Conv2D(64,(10,10),activation='relu',input_shape=input_shape,
+                   kernel_initializer=W_init,kernel_regularizer=l2(2e-4)))
+convnet.add(MaxPooling2D())
+convnet.add(Conv2D(128,(7,7),activation='relu',
+                   kernel_regularizer=l2(2e-4),kernel_initializer=W_init,bias_initializer=b_init))
+convnet.add(MaxPooling2D())
+convnet.add(Conv2D(128,(4,4),activation='relu',kernel_initializer=W_init,kernel_regularizer=l2(2e-4),bias_initializer=b_init))
+convnet.add(MaxPooling2D())
+convnet.add(Conv2D(256,(4,4),activation='relu',kernel_initializer=W_init,kernel_regularizer=l2(2e-4),bias_initializer=b_init))
+convnet.add(Flatten())
+convnet.add(Dense(4096,activation="sigmoid",kernel_regularizer=l2(1e-3),kernel_initializer=W_init,bias_initializer=b_init))
+
+#call the convnet Sequential model on each of the input tensors so params will be shared
+encoded_l = convnet(left_input)
+encoded_r = convnet(right_input)
+#layer to merge two encoded inputs with the l1 distance between them
+L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
+#call this layer on list of two input tensors.
+L1_distance = L1_layer([encoded_l, encoded_r])
+prediction = Dense(1,activation='sigmoid',bias_initializer=b_init)(L1_distance)
+siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
+
+optimizer = Adam(0.00006)
+#//TODO: get layerwise learning rates and momentum annealing scheme described in paperworking
+siamese_net.compile(loss="binary_crossentropy",optimizer=optimizer)
+
+siamese_net.count_params()
+
+
+# In[2]:
+
+
+PATH = "/home/abhishek/projects/bad_siamese/poisoned_data" #CHANGE THIS - path where the pickled data is stored
+
+with open(os.path.join(PATH, "train.pickle"), "rb") as f:
+    (X,c) = pickle.load(f)
+
+with open(os.path.join(PATH, "val.pickle"), "rb") as f:
+    (Xval,cval) = pickle.load(f)
+
+print("training alphabets")
+print(c.keys())
+print("validation alphabets:")
+print(cval.keys())
+
+
+# In[3]:
+
+
+class Siamese_Loader:
+    """For loading batches and testing tasks to a siamese net"""
+    def __init__(self, path, data_subsets = ["train", "val"]):
+        self.data = {}
+        self.categories = {}
+        self.info = {}
+
+        for name in data_subsets:
+            file_path = os.path.join(path, name + ".pickle")
+            print("loading data from {}".format(file_path))
+            with open(file_path,"rb") as f:
+                (X,c) = pickle.load(f)
+                self.data[name] = X
+                self.categories[name] = c
+
+    def get_batch(self,batch_size,s="train"):
+        """Create batch of n pairs, half same class, half different class"""
+        X=self.data[s]
+        n_classes, n_examples, w, h = X.shape
+
+        #randomly sample several classes to use in the batch
+        categories = rng.choice(n_classes,size=(batch_size,),replace=False)
+        #initialize 2 empty arrays for the input image batch
+        pairs=[np.zeros((batch_size, h, w,1)) for i in range(2)]
+        #initialize vector for the targets, and make one half of it '1's, so 2nd half of batch has same class
+        targets=np.zeros((batch_size,))
+        targets[batch_size//2:] = 1
+        for i in range(batch_size):
+            category = categories[i]
+            idx_1 = rng.randint(0, n_examples)
+            pairs[0][i,:,:,:] = X[category, idx_1].reshape(w, h, 1)
+            idx_2 = rng.randint(0, n_examples)
+            #pick images of same class for 1st half, different for 2nd
+            if i >= batch_size // 2:
+                category_2 = category
+            else:
+                #add a random number to the category modulo n classes to ensure 2nd image has
+                # ..different category
+                category_2 = (category + rng.randint(1,n_classes)) % n_classes
+            pairs[1][i,:,:,:] = X[category_2,idx_2].reshape(w, h,1)
+        return pairs, targets
+
+    def generate(self, batch_size, s="train"):
+        """a generator for batches, so model.fit_generator can be used. """
+        while True:
+            pairs, targets = self.get_batch(batch_size,s)
+            yield (pairs, targets)
+
+    def make_oneshot_task(self,N,s="val",language=None):
+        """Create pairs of test image, support set for testing N way one-shot learning. """
+        X=self.data[s]
+        n_classes, n_examples, w, h = X.shape
+        indices = rng.randint(0,n_examples,size=(N,))
+        if language is not None:
+            low, high = self.categories[s][language]
+            if N > high - low:
+                raise ValueError("This language ({}) has less than {} letters".format(language, N))
+            categories = rng.choice(range(low,high),size=(N,),replace=False)
+
+        else:#if no language specified just pick a bunch of random letters
+            categories = rng.choice(range(n_classes),size=(N,),replace=False)
+        true_category = categories[0]
+        ex1, ex2 = rng.choice(n_examples,replace=False,size=(2,))
+        test_image = np.asarray([X[true_category,ex1,:,:]]*N).reshape(N, w, h,1)
+        support_set = X[categories,indices,:,:]
+        support_set[0,:,:] = X[true_category,ex2]
+        support_set = support_set.reshape(N, w, h,1)
+        targets = np.zeros((N,))
+        targets[0] = 1
+        targets, test_image, support_set = shuffle(targets, test_image, support_set)
+        pairs = [test_image,support_set]
+
+        return pairs, targets
+
+    def test_oneshot(self,model,N,k,s="val",verbose=0):
+        """Test average N way oneshot learning accuracy of a siamese neural net over k one-shot tasks"""
+        n_correct = 0
+        for i in range(k):
+            inputs, targets = self.make_oneshot_task(N,s)
+            probs = model.predict(inputs)
+            if np.argmax(probs) == np.argmax(targets):
+                n_correct+=1
+        percent_correct = (100.0*n_correct / k)
+        if verbose:
+            print"current accuracy on random data of 250 samples :",percent_correct
+        return percent_correct
+
+    def train(self, model, epochs, verbosity):
+        model.fit_generator(self.generate(batch_size),
+
+                             )
+
+
+#Instantiate the class
+loader = Siamese_Loader(PATH)
+
+
+# In[6]:
+
+
+siamese_net.load_weights('./poisoned_data/poisoned_weights')
+
+
+# In[ ]:
+
+
+ways = np.arange(1, 60, 2)
+resume =  False
+val_accs, train_accs,nn_accs = [], [], []
+trials = 450
+for N in ways:
+    val_accs.append(loader.test_oneshot(siamese_net, N,trials, "val", verbose=True))
+    train_accs.append(loader.test_oneshot(siamese_net, N,trials, "train", verbose=True))
+    print "  ",N
+
+#plot the accuracy vs num categories for each
+plt.plot(ways, val_accs, "m")
+plt.plot(ways, train_accs, "y")
+
+
+# In[ ]:
+
+
+plt.savefig('/home/abhishek/Pictures/just_poisoned.jpg')
+plt.clf()
+
+
+# In[ ]:
+
+
+PATH = "/home/abhishek/projects/bad_siamese/poisoned_data" #CHANGE THIS - path where the pickled data is stored
+
+with open(os.path.join(PATH, "train.pickle"), "rb") as f:
+    (X,c) = pickle.load(f)
+
+with open(os.path.join(PATH, "val.pickle"), "rb") as f:
+    (Xval,cval) = pickle.load(f)
+
+print("training alphabets")
+print(c.keys())
+print("validation alphabets:")
+print(cval.keys())
+
+
+# In[ ]:
+
+
+siamese_net.load_weights('./true_data/weights')
+
+
+# In[ ]:
+
+
+ways = np.arange(1, 60, 2)
+resume =  False
+val_accs, train_accs,nn_accs = [], [], []
+trials = 450
+for N in ways:
+    val_accs.append(loader.test_oneshot(siamese_net, N,trials, "val", verbose=True))
+    train_accs.append(loader.test_oneshot(siamese_net, N,trials, "train", verbose=True))
+    print "  ",N
+
+#plot the accuracy vs num categories for each
+plt.plot(ways, val_accs, "m")
+plt.plot(ways, train_accs, "y")
+
+# In[ ]:
+
+
+plt.savefig('/home/abhishek/Pictures/just_true.jpg')
+#plt.show()
+print "DONE"
+
